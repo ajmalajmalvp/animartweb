163d8dda6145d8ef47d0b6d8c376bcc069ce13ba
Ajmal <109466213+ajmalajmalvp@users.noreply.github.com>
2026-01-15 20:36:03 +0530

    updated

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,13 +3,13 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js')
 // // Initialize the Firebase app in the service worker by passing the generated config
 
 const firebaseConfig = {
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
-    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
-    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
-    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
-    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
-    measurementId: "xxxxxxxxxxxxxxxxxxxxxxxxxx"
+  apiKey: "AIzaSyCcd0h02NBoosa3tudgV19CPWNuyLgK07Q",
+  authDomain: "animart-8c911.firebaseapp.com",
+  projectId: "animart-8c911",
+  storageBucket: "animart-8c911.firebasestorage.app",
+  messagingSenderId: "700088778140",
+  appId: "1:700088778140:web:33e0657080b80cc04cdc59",
+  measurementId: "G-9WF2J0X85B"
 };
 
 firebase?.initializeApp(firebaseConfig)
